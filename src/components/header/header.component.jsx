2cd79b08230bbeba4aae2050143a6dc2ea15968a
Anish Bhattacherjee <anishbhattacherjee@Anishs-MacBook-Air.local>
2023-03-27 15:07:42 +0530

    cart icon, cart dropdown and cart reducer implemented

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import { connect } from "react-redux";
 import './header.styles.scss';
 import { ReactComponent as Logo} from '../../assets/crown.svg'
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser , hidden}) => (
     <div className="header">
         <NavLink className="logo-container" to="/">
             <Logo className="logo" />
@@ -29,11 +31,15 @@ const Header = ({currentUser}) => (
                 </NavLink>
 
             }
+            <CartIcon />
         </div>
+        { hidden ? null :  <CartDropdown /> }
+       
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser}, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header);
